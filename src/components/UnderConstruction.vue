1408799ca43e20b84c73e0984189ba467dbcb2e6
Julian Grey <julianrgrey@gmail.com>
2020-08-19 13:09:12 +0900

    Text alignment

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -25,6 +25,7 @@ export default Vue.extend({});
   &__content {
     display: flex;
     align-items: center;
+    text-align: center;
     justify-content: center;
     flex-direction: column;
   }
