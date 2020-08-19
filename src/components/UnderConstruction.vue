a8e51dc825b81ec2a292c72b7dbfaf21d5b4e35d
Julian Grey <julianrgrey@gmail.com>
2020-08-19 12:33:02 +0900

    Delete Vue favicon, run lint

--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({});
 </script>
 
@@ -7,7 +7,10 @@ export default Vue.extend({});
   <div class="under-construction">
     <div class="under-construction__content">
       <h1>Under Construction</h1>
-      <p>This site is currently undergoing a complete rewrite using the Vue framework.</p>
+      <p>
+        This site is currently undergoing a complete rewrite using the Vue
+        framework.
+      </p>
       <p>Please check back shortly.</p>
       <p>I am sorry for the inconvenience.</p>
     </div>
@@ -15,15 +18,15 @@ export default Vue.extend({});
 </template>
 
 <style lang="scss" scoped>
-  .under-construction {
-    height: 100vh;
-    width: 100vw;
+.under-construction {
+  height: 100vh;
+  width: 100vw;
 
-    &__content {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-    }
+  &__content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
   }
+}
 </style>
